1463390c4b604695a881185d0f00d3d994ff5117
Josh Hayashida <josh.hayashida@gmail.com>
2020-07-16 22:44:36 -0400

    email exception

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+    email: {
+      email_prefix: '[PREFIX] ',
+      sender_address: %{"notifier" <ZZZemail>},
+      exception_recipients: %w{ZZZemail}
+    }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
