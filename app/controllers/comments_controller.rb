bc42c60ba1e251da4fb0527d73130bcb7727a098
Josh Hayashida <josh.hayashida@gmail.com>
2020-07-16 22:06:09 -0400

    update query

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def index
     if (params.has_key?(:q))
-      @comments = Comment.where("text_display LIKE ?", "%#{params[:q]}%").order(comment_published_at: :desc).limit(10)
+      @comments = Comment.where("lower(text_display) LIKE ?", "%#{params[:q].gsub("'", "&#39;").downcase}%").order(comment_published_at: :desc).limit(10)
     else
       @comments = Comment.order(comment_published_at: :desc).limit(10)
     end
