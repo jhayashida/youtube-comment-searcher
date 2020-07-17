f808a1df43a4fc84623b833cb507039d4e235c25
Josh Hayashida <josh.hayashida@gmail.com>
2020-07-17 12:00:48 -0400

    select between date ranges and pagination

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,21 @@
 class CommentsController < ApplicationController
   def index
+    @comments = Comment.all
+
     if (params.has_key?(:q))
-      @comments = Comment.where("lower(text_display) LIKE ?", "%#{params[:q].gsub("'", "&#39;").downcase}%").order(comment_published_at: :desc).limit(10)
-    else
-      @comments = Comment.order(comment_published_at: :desc).limit(10)
+      @comments = @comments.where("lower(text_display) LIKE ?", "%#{params[:q].gsub("'", "&#39;").downcase}%")
     end
+
+    if params.has_key?(:after) && params.has_key?(:before)
+      @comments = @comments.where(comment_published_at: params[:after].to_date.beginning_of_day..params[:before].to_date.end_of_day)
+    elsif !params.has_key?(:after) && params.has_key?(:before)
+      @comments = @comments.where(comment_published_at: "01/01/1900".to_date..params[:before].to_date)
+    elsif params.has_key?(:after) && !params.has_key?(:before)
+      @comments = @comments.where(comment_published_at: params[:after].to_date..Date.today)
+    else
+
+    end
+
+    @comments = @comments.order(comment_published_at: :desc).paginate(page: params[:page], per_page: 30)
   end
 end
